941d99aa4b9ddc062d7f5a4cc4406192a5ad4167
superjoachim <joachim@stapelfeldt.com>
2018-01-30 16:48:41 +0100

    Opdateret deployment

--- Deploy.ps1
+++ Deploy.ps1
@@ -18,6 +18,10 @@ $Token = Get-Content "$PathToStaticStuff\token.txt"
 (Get-Content "$PathToBot\config\bot.json").Replace("<your token>", $Token) | Set-Content "$PathToBot\config\bot.json" -Verbose
 $botID = Get-Content "$PathToStaticStuff\botid.txt"
 (Get-Content "$PathToBot\config\bot.json").Replace("<your bot id>", $botID) | Set-Content "$PathToBot\config\bot.json" -Verbose
+$csgoIP = Get-Content "$PathToStaticStuff\csgoip.txt"
+(Get-Content "$PathToBot\config\bot.json").Replace("<your csgo server ip>", $csgoIP) | Set-Content "$PathToBot\config\bot.json" -Verbose
+$rconpassword = Get-Content "$PathToStaticStuff\rconpassword.txt"
+(Get-Content "$PathToBot\config\bot.json").Replace("<your rcon password>", $rconpassword) | Set-Content "$PathToBot\config\bot.json" -Verbose
 
 "Start bot"
 Start-Service -Name "whatbot"
